f0a82ef7d17bae5f3626a1adf7a61dc937a64cb8
Wim Lemmens <wim.lemmens@gmail.com>
2025-01-10 11:40:16 +0100

    WIP: a tasks table with filter is visible

--- src/app/components/task-list/task-list.component.ts
+++ src/app/components/task-list/task-list.component.ts
@@ -3,9 +3,13 @@ import { TaskService } from '../../services/task.service';
 import { TaskFormComponent } from '../task-form/task-form.component';
 import { UtilsService } from '../../services/utils.service';
 
+import {MatTableDataSource, MatTableModule} from '@angular/material/table';
+import {MatInputModule} from '@angular/material/input';
+import {MatFormFieldModule} from '@angular/material/form-field';
+
 @Component({
   selector: 'app-task-list',
-  imports: [TaskFormComponent],
+  imports: [TaskFormComponent, MatFormFieldModule, MatInputModule, MatTableModule],
   templateUrl: './task-list.component.html',
   styleUrl: './task-list.component.css'
 })
@@ -15,6 +19,19 @@ export class TaskListComponent {
   
   utils = inject(UtilsService);
 
+
+  // Material table features
+  displayedColumns: string[] = ['id', 'title', 'deadline', 'estimate', 'feeling'];
+  dataSource = new MatTableDataSource(this.tasks());
+  applyFilter(event: Event) {
+    console.log(event)
+    const filterValue = (event.target as HTMLInputElement).value;
+    this.dataSource.filter = filterValue.trim().toLowerCase();
+  }
+  // END Material table features
+
+
+
   editTaskId:number = 0;
   showTaskForm:boolean = false;
 
@@ -23,6 +40,8 @@ export class TaskListComponent {
     effect(() => {
       console.log('Tasks updated effect', this.tasks());
       this.utils.toast("Tasks list updated", "info");
+
+      this.dataSource = new MatTableDataSource(this.tasks());
     });
   }
 
