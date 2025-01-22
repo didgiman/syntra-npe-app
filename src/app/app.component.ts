b09a8e598a21c4e6de455513dd06179e0e3903de
Wim Lemmens <wim.lemmens@gmail.com>
2025-01-22 15:18:49 +0100

    WIP: darkmode switch

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,10 +2,11 @@ import { Component, inject } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { TaskListComponent } from './components/task-list/task-list.component';
 import { UtilsService } from './services/utils.service';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, TaskListComponent],
+  imports: [RouterOutlet, TaskListComponent, CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
@@ -13,8 +14,21 @@ export class AppComponent {
   utils = inject(UtilsService);
   title = `What's Next`;
 
+  darkModeActive = true;
+
   constructor() {
     this.utils.toast("Welcome to Syntra NPE's - What's Next", "warning");
+  }
+
+  toggleDarkMode() {
+    const htmlElement = document.documentElement;
+    if (htmlElement.classList.contains('dark')) {
+      htmlElement.classList.remove('dark');
+    } else {
+      htmlElement.classList.add('dark');
+    }
+
+    this.darkModeActive = !this.darkModeActive;
   }
   
 }
