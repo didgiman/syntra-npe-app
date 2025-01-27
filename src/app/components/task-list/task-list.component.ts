c8796811e0f0222f99f71b8f1b24d1d0f94aae3a
Wim Lemmens <wim.lemmens@gmail.com>
2025-01-27 19:58:04 +0100

    Fixed error when creating first task for a new user

--- src/app/components/task-list/task-list.component.ts
+++ src/app/components/task-list/task-list.component.ts
@@ -119,8 +119,10 @@ export class TaskListComponent {
   }
 
   sortTable(field: string, order: number) {
-    this.table.sortField = field;
-    this.table.sortOrder = order;
+    if (this.table) {
+      this.table.sortField = field;
+      this.table.sortOrder = order;
+    }
   }
 
   toggleFinishedTasks(event: any) {
